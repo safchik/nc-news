6ae16db02850810ad9f33632be5211c908fed80f
safchik <safchik9@gmail.com>
2023-04-18 11:41:19 +0100

    view of list of all artickles on homepage added

--- src/api.js
+++ src/api.js
@@ -1,9 +1,11 @@
-// import axios from "axios";
+import axios from "axios";
 
 
-// export const fetchArticles = (article_id) => {
-//     return axios
-//         .get(`https://nc-news-project5.onrender.com/api/articles`)
-//         .then(({ data: { article } }) => article)
-// }
+export const fetchAllArticles = (article) => {
+    return axios
+        .get(`https://nc-news-project5.onrender.com/api/articles`)
+        .then((response) => {
+            return response.data.articles;
+        });
+}
 
--- src/components/AllArticles.jsx
+++ src/components/AllArticles.jsx
@@ -1,36 +1,43 @@
 import { useEffect, useState } from "react";
+import { fetchAllArticles } from "../api";
+
 
 function AllArticles() {
-    const [articles, setArticles] = useState([]);
+
+    const [isLoading, setLoading] = useState(true);
+    const [list, setList] = useState([]);
 
     useEffect(() => {
-        fetch(`https://nc-news-project5.onrender.com/api/articles`)
-            .then((response) => {
-                return response.json();
-            })
-            .then((data) => {
-                setArticles(data.articles);
-            });
+        fetchAllArticles().then((articles) => {
+            setList(articles);
+            setLoading(false);
+        })
     }, []);
 
     return (
-        <div>
-            <h2>Articles</h2>
-            {articles.map((article) => {
-                return (
-                    <div key={article.article_id}>
-                        <p>Title: {article.title}</p>
-                        <p>Topic: {article.topic}</p>
-                        <p>Author: {article.author}</p>
-                        <img src={article.article_img_url}></img>
-                        <p>Body: {article.body}</p>
-                        <p>Comments: {article.comments}</p>
-                        <p>Votes: {article.votes}</p>
-                        <p>Created at: {article.created_at}</p>
-                    </div>
-                );
-            })}
-        </div>
+        <section id="articles">
+            <h2>All Articles</h2>
+            {isLoading ? (
+                <h3>Loading...</h3>
+            ) : (
+                <ul>
+                    {list.map((article) => {
+                        return (
+                            <li key={article.article_id}>
+                                <p>Title: {article.title}</p>
+                                <p>Topic: {article.topic}</p>
+                                <p>Author: {article.author}</p>
+                                <p>Body: {article.body}</p>
+                                <p>Created at: {article.created_at}</p>
+                                <p>Votes: {article.votes}</p>
+                                <p>Comments: {article.comments}</p>
+                                <img src={article.article_img_url}></img>
+                            </li>
+                        );
+                    })}
+                </ul>
+            )}
+        </section>
     );
 }
 
