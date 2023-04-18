6bea3833f69f907240be6f5a457aa8827ca210a9
safchik <safchik9@gmail.com>
2023-04-18 11:08:31 +0100

    view of list of all artickles added

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-import '@babel/polyfill';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
